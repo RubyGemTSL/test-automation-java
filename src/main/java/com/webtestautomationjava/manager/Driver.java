676b54e5cf4badaba875023fd49215ec101db5be
ianhamilton <i.hamilton@accenture.com>
2015-05-12 15:27:13 +0100

    Updated Driver to be singleton.

--- src/main/java/com/webtestautomationjava/manager/Driver.java
+++ src/main/java/com/webtestautomationjava/manager/Driver.java
@@ -5,32 +5,37 @@ import org.openqa.selenium.WebDriver;
 
 public class Driver extends Thread {
 
+    private static WebDriver selectedDriver = null;
+
     public static final String BROWSER = "browser";
 
     public static WebDriver get() {
 
-        String browserToUse="";
+        String browserToUse = "";
 
-        if(System.getProperties().containsKey(BROWSER)){
+        if (System.getProperties().containsKey(BROWSER)) {
             browserToUse = System.getProperty(BROWSER);
         }
+        if (selectedDriver == null) {
+            switch (browserToUse) {
+                case "firefox":
+                    selectedDriver = DriverManager.createFirefoxDriver();
+                case "htmlunit":
+                    selectedDriver = DriverManager.createHtmlUnitDriver();
+                case "chrome":
+                    selectedDriver = DriverManager.createChromeDriver();
+                case "iphone5s":
+                    selectedDriver = DriverManager.createIphone5Driver();
+                case "ipadair":
+                    selectedDriver = DriverManager.createIPadAirDriver();
+                case "nexus7":
+                    selectedDriver = DriverManager.createAndroidNexus7Driver();
+                default:
+                    selectedDriver = DriverManager.createFirefoxDriver();
+            }
 
-        switch(browserToUse) {
-            case "firefox":
-                return DriverManager.createFirefoxDriver();
-            case "htmlunit":
-                return DriverManager.createHtmlUnitDriver();
-            case "chrome":
-                return DriverManager.createChromeDriver();
-            case "iphone5s":
-                return DriverManager.createIphone5Driver();
-            case "ipadair":
-                return DriverManager.createIPadAirDriver();
-            case "nexus7":
-                return DriverManager.createAndroidNexus7Driver();
-            default:
-                return DriverManager.createFirefoxDriver();
         }
 
+        return selectedDriver;
     }
 }
